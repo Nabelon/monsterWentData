a7e0c9c891b524224baf0cf5ef22efde059f5f73
Ben2 <haho.hoha@web.de>
2017-07-22 22:34:05 +0200

    added cat to attacksortet

--- movesSort.py
+++ movesSort.py
@@ -8,10 +8,10 @@ def main():
         s = str(i)
         print s
         if oldDic[s]["type"] in newDic.keys():
-            newDic[oldDic[s]["type"]].append(oldDic[s]["name"] + " Id:" + s + " Str:" + str(oldDic[s]["dmg"]) + " Acc:" + str(oldDic[s]["acc"]))
+            newDic[oldDic[s]["type"]].append(oldDic[s]["name"] + " Id:" + s + " Str:" + str(oldDic[s]["dmg"]) + " Acc:" + str(oldDic[s]["acc"]) + " cat: " + str(oldDic[s]["cat"]))
         else:
             m = []
-            m.append(oldDic[s]["name"] + " Id:" + s + " Str:" + str(oldDic[s]["dmg"]) + " Acc:" + str(oldDic[s]["acc"]))
+            m.append(oldDic[s]["name"] + " Id:" + s + " Str:" + str(oldDic[s]["dmg"]) + " Acc:" + str(oldDic[s]["acc"]) + " cat: " + str(oldDic[s]["cat"]))
             newDic[oldDic[s]["type"]] = m
     with open("movesSorted.json", 'w') as f:
             json.dump(newDic, f, indent=2, sort_keys=True)
