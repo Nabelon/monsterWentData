7d4fcc265e6faeae960c63be63b86271780647ca
Ben2 <haho.hoha@web.de>
2017-07-23 11:53:06 +0200

    avg stats.py was wrong

--- statAvg.py
+++ statAvg.py
@@ -8,7 +8,7 @@ def main():
         s = str(i)
         print s
         mDic = oldDic[s]["baseStats"]
-        newDic['{0:05d}'.format(i)] = mDic["special_attack"]+mDic["special_attack"]+mDic["hp"]+mDic["attack"]+mDic["defense"]+mDic["speed"]+mDic["special_defense"]
+        newDic['{0:05d}'.format(i)] = mDic["mAtk"]+mDic["hp"]+mDic["atk"]+mDic["armor"]+mDic["speed"]+mDic["mRes"]
     with open("monsterStatsAvg.json", 'w') as f:
             json.dump(newDic, f, indent=2, sort_keys=True)
 if __name__ == '__main__':
